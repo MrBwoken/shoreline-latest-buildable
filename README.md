92384b4224a309dc748647a23f1a99fa465ab5ac
not-filepile <pjwearth1@gmail.com>
2026-04-01 01:39:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[https://github.com/lolwut2/Shoreline-V2](https://github.com/lolwut2/Shoreline-V2)
+---
+$25 exit-scammed client.  
+thx to lolwut for uploading the code(unbuildable)
+
+Not all features have been tested. 
+Chams was not. working originally; only the crash has been prevented for now
+
+![screenshot](image.png)
---
+++ src/main/java/net/shoreline/client/mixin/render/entity/MixinEndCrystalEntityRenderer.java
@@ -0,0 +1,110 @@
+package net.shoreline.client.mixin.render.entity;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EndCrystalEntityRenderer;
+import net.minecraft.client.render.entity.model.EndCrystalEntityModel;
+import net.minecraft.client.render.entity.state.EndCrystalEntityRenderState;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.decoration.EndCrystalEntity;
+import net.minecraft.util.Identifier;
+import net.shoreline.client.ShorelineMod;
+import net.shoreline.client.impl.imixin.IModel;
+import net.shoreline.client.impl.module.render.ChamsModule;
+import net.shoreline.client.impl.module.render.ModelsModule;
+import net.shoreline.client.impl.render.ChamsRenderer;
+import net.shoreline.client.impl.render.ColorUtil;
+import net.shoreline.client.impl.render.Layers;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EndCrystalEntityRenderer.class)
+public class MixinEndCrystalEntityRenderer
+{
+    @Mutable
+    @Shadow
+    @Final
+    private static RenderLayer END_CRYSTAL;
+
+    @Shadow
+    @Final
+    private static Identifier TEXTURE;
+
+    @Shadow
+    @Final
+    private EndCrystalEntityModel model;
+
+    @Unique
+    private EndCrystalEntity last;
+
+    @Unique
+    private static final Identifier BLANK = Identifier.of(ShorelineMod.MOD_ID, "textures/blank.png");
+
+    @Inject(
+            method = "updateRenderState(Lnet/minecraft/entity/decoration/EndCrystalEntity;" +
+                    "Lnet/minecraft/client/render/entity/state/EndCrystalEntityRenderState;F)V",
+            at = @At(value = "HEAD"))
+    private void updateRenderStateHook(EndCrystalEntity endCrystalEntity,
+                                       EndCrystalEntityRenderState endCrystalEntityRenderState,
+                                       float f,
+                                       CallbackInfo info)
+    {
+        this.last = endCrystalEntity;
+    }
+
+    @Inject(
+            method = "render(Lnet/minecraft/client/render/entity/state/EndCrystalEntityRenderState;" +
+                    "Lnet/minecraft/client/util/math/MatrixStack;" +
+                    "Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/entity/model/EndCrystalEntityModel;" +
+                            "setAngles(Lnet/minecraft/client/render/entity/state/EndCrystalEntityRenderState;)V"))
+    private void preModelRenderHook(EndCrystalEntityRenderState endCrystalEntityRenderState,
+                                    MatrixStack matrixStack,
+                                    VertexConsumerProvider vertexConsumerProvider,
+                                    int i,
+                                    CallbackInfo info)
+    {
+        if (ChamsModule.getInstance().isEnabled() && ChamsModule.getInstance().isValid(last))
+        {
+            END_CRYSTAL = Layers.CRYSTALS.apply(ChamsModule.getInstance().getOpacity() == 0.0f ? BLANK : TEXTURE, true);
+            return;
+        }
+
+        END_CRYSTAL = RenderLayer.getEntityCutoutNoCull(TEXTURE);
+    }
+
+
+    @Inject(
+            method = "render(Lnet/minecraft/client/render/entity/state/EndCrystalEntityRenderState;" +
+                    "Lnet/minecraft/client/util/math/MatrixStack;" +
+                    "Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/entity/model/EndCrystalEntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V",
+                    shift = At.Shift.AFTER))
+    private void setAnglesHook(EndCrystalEntityRenderState endCrystalEntityRenderState, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info)
+    {
+        // Disabled for 1.21.4: the extra Chams render pass breaks buffer builder state.
+    }
+
+    @Redirect(
+            method = "render(Lnet/minecraft/client/render/entity/state/EndCrystalEntityRenderState;" +
+                    "Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/util/math/MatrixStack;scale(FFF)V"))
+    private void hookScale(MatrixStack instance, float x, float y, float z)
+    {
+        float scale = ModelsModule.INSTANCE.isEnabled() ? ModelsModule.INSTANCE.getCrystalScale().getValue() : 1.0f;
+        instance.scale(2.0f * scale, 2.0f * scale, 2.0f * scale);
+    }
+}
